2e38f311e61a1d9ad5d49584476284c323b71ebc
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2018-09-21 18:08:04 +0200

    Improvements for supporting WFS source in openlayers vector layers, and clustering of points too

--- web/client/components/map/openlayers/plugins/VectorLayer.js
+++ web/client/components/map/openlayers/plugins/VectorLayer.js
@@ -7,11 +7,14 @@
  */
 
 var Layers = require('../../../../utils/openlayers/Layers');
+var SecurityUtils = require('../../../../utils/SecurityUtils');
 var markerIcon = require('../img/marker-icon.png');
 var markerShadow = require('../img/marker-shadow.png');
 var ol = require('openlayers');
+var url = require('url');
 
 const assign = require('object-assign');
+const {isEqual} = require('lodash');
 
 const image = new ol.style.Circle({
   radius: 5,
@@ -104,26 +107,76 @@ var styleFunction = function(feature) {
     return defaultStyles[feature.getGeometry().getType()];
 };
 
+var getSource = function(opts) {
+    if (opts.wfs) {
+        const source = new ol.source.Vector({
+            loader: function(extent, resolution, projection) {
+                const options = this.get('options');
+                const proj = projection.getCode();
+                let queryParameters = assign({
+                    service: 'WFS',
+                    version: '1.0.0',
+                    request: 'GetFeature',
+                    typeName: options.name,
+                    outputFormat: 'application/json',
+                    proj: proj
+                }, options.params);
+                queryParameters.cql_filter += ' AND BBOX(geom,' + extent.join(',') + ')';
+                const wfsurl = options.wfs[0] + '/wfs' + url.format({
+                    query: queryParameters
+                });
+                const xhr = new XMLHttpRequest();
+                xhr.open('GET', wfsurl);
+                const onError = function() {
+                    source.removeLoadedExtent(extent);
+                };
+                xhr.onerror = onError;
+                xhr.onload = function() {
+                    if (xhr.status === 200) {
+                        source.addFeatures(
+                            source.getFormat().readFeatures(xhr.responseText));
+                    } else {
+                        onError();
+                    }
+                };
+                xhr.send();
+            },
+            strategy: function(extent) {
+                return [extent];
+            },
+            format: new ol.format.GeoJSON()
+        });
+        source.set('options', opts);
+        if (opts.cluster) {
+            return new ol.source.Cluster({
+                distance: 30,
+                source: source
+            });
+        }
+        return source;
+    }
+    let features;
+    let featuresCrs = opts.featuresCrs || 'EPSG:4326';
+    let layerCrs = opts.crs || 'EPSG:3857';
+    if (opts.features) {
+        let featureCollection = opts.features;
+        if (Array.isArray(opts.features)) {
+            featureCollection = { "type": "FeatureCollection", features: featureCollection };
+        }
+        features = (new ol.format.GeoJSON()).readFeatures(featureCollection);
+        if (featuresCrs !== layerCrs) {
+            features.forEach((f) => f.getGeometry().transform(featuresCrs, layerCrs));
+        }
+    }
+
+    return new ol.source.Vector({
+        features: features
+    });
+};
+
 Layers.registerType('vector', {
     create: (options) => {
-        let features;
-        let featuresCrs = options.featuresCrs || 'EPSG:4326';
-        let layerCrs = options.crs || 'EPSG:3857';
-        if (options.features) {
-            let featureCollection = options.features;
-            if (Array.isArray(options.features)) {
-                featureCollection = { "type": "FeatureCollection", features: featureCollection};
-            }
-            features = (new ol.format.GeoJSON()).readFeatures(featureCollection);
-            if (featuresCrs !== layerCrs) {
-                features.forEach((f) => f.getGeometry().transform(featuresCrs, layerCrs));
-            }
-        }
-
-        const source = new ol.source.Vector({
-            features: features
-        });
-
+        const source = getSource(options);
         let style = options.nativeStyle;
         if (!style && options.style) {
             style = {
@@ -155,6 +208,36 @@ Layers.registerType('vector', {
 
             style = new ol.style.Style(style);
         }
+        const styleCache = {};
+        if (!style && options.cluster) {
+            style = function(feature) {
+                const size = feature.get('features').length;
+                let currentStyle = styleCache[size];
+                if (!currentStyle) {
+                    currentStyle = new ol.style.Style({
+                        image: new ol.style.Circle({
+                            radius: 15,
+                            stroke: new ol.style.Stroke({
+                                color: '#C64145',
+                                opacity: 0.3,
+                                width: 5
+                            }),
+                            fill: new ol.style.Fill({
+                                color: '#BA2536'
+                            })
+                        }),
+                        text: new ol.style.Text({
+                            text: size.toString(),
+                            fill: new ol.style.Fill({
+                                color: '#fff'
+                            })
+                        })
+                    });
+                    styleCache[size] = currentStyle;
+                }
+                return currentStyle;
+            };
+        }
 
         return new ol.layer.Vector({
             msId: options.id,
@@ -170,6 +253,11 @@ Layers.registerType('vector', {
             layer.getSource().forEachFeature((f) => {
                 f.getGeometry().transform(oldCrs, newCrs);
             });
+        }
+        if (!isEqual(newOptions.params, oldOptions.params)) {
+            layer.getSource().set('options', newOptions);
+            layer.getSource().clear();
+            layer.getSource().refresh();
         }
     },
     render: () => {
